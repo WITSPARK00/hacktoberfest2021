4189d80b83fb93b53489e1f030dce939475cf776
WITSPARK00 <116668873+WITSPARK00@users.noreply.github.com>
2022-10-25 22:57:24 +0530

    Create Pro.py

---
+++ Pro.py
@@ -0,0 +1,11 @@
+# Python3 code to demonstrate variable assignment
+# upon condition using Conditional Operator
+
+# Initialising variables using Conditional Operator
+a = 1 if 20 > 10 else 0
+
+# Printing value of a
+print("The value of a is: " , str(a))
+
+# This code is contributed by shivanisinghss2110
+
